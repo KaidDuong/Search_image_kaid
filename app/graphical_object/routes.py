708abaa2635c69bde77fb847316e241344f29435
KaidDuong <cooleen.cl@gmail.com>
2020-12-10 19:57:59 +0700

    test

--- app/graphical_object/routes.py
+++ app/graphical_object/routes.py
@@ -65,26 +65,26 @@ def index_images():
         # grab the paths to the input images and initialize the dictionary
         # of hashes
 
-        # imagePaths = list(paths.list_images(os.path.join(basedir, 'static','images')))
-        # hashes = {}
-        # print(imagePaths)
-        # # loop over the image paths
-        # for (i, imagePath) in enumerate(imagePaths):
-	    #     # load the input image
-	    #     print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
-	    #     image = cv2.imread(imagePath)
-	    #     # compute the hash for the image and convert it
-	    #     h = dhash(image)
-	    #     h = convert_hash(h)
-	    #     # update the hashes dictionary
-	    #     l = hashes.get(h, [])
-	    #     l.append(imagePath)
-	    #     hashes[h] = l
+        imagePaths = list(paths.list_images(os.path.join(basedir, 'static','images')))
+        hashes = {}
+        print(imagePaths)
+        # loop over the image paths
+        for (i, imagePath) in enumerate(imagePaths):
+	        # load the input image
+	        print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+	        image = cv2.imread(imagePath)
+	        # compute the hash for the image and convert it
+	        h = dhash(image)
+	        h = convert_hash(h)
+	        # update the hashes dictionary
+	        l = hashes.get(h, [])
+	        l.append(imagePath)
+	        hashes[h] = l
 
-        # # build the VP-Tree
-        # print("[INFO] building VP-Tree...")
-        # points = list(hashes.keys())
-        # tree = vptree.VPTree(points, hamming)
+        # build the VP-Tree
+        print("[INFO] building VP-Tree...")
+        points = list(hashes.keys())
+        tree = vptree.VPTree(points, hamming)
 
         # # serialize the VP-Tree to disk
         # print("[INFO] serializing VP-Tree...")
